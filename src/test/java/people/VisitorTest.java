40bd69183bfb6fd77246fa9a7bce8d2610e42519
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-02 18:57:08 +0100

    add test for visited attraction, fails because we still need to write the method

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,8 @@
 package people;
 
+import attractions.Dodgems;
+import attractions.Playground;
+import attractions.RollerCoaster;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +11,14 @@ import static org.junit.Assert.assertEquals;
 public class VisitorTest {
 
     Visitor visitor;
+    Dodgems dodgems;
+    RollerCoaster rollerCoaster;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        dodgems = new Dodgems("dodgem", 8);
+        rollerCoaster = new RollerCoaster("roller", 9);
     }
 
     @Test
@@ -27,5 +34,11 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void  canAddAttractions(){
+        visitor.addAttraction(rollerCoaster);
+        assertEquals(1, visitor.countVisitedAttractions());
     }
 }
