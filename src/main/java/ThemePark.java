d00e9bbde5ae3ab92e80b123889d46fe9452e885
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-02 18:05:47 +0100

    create constructor in ThemePark

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,23 @@
+import attractions.Attraction;
+import stalls.Stall;
+
 public class ThemePark {
+
+    Attraction attractions;
+    Stall stalls;
+
+    public ThemePark(Attraction attractions, Stall stalls) {
+        this.attractions = attractions;
+        this.stalls = stalls;
+    }
+
+    public Attraction getAttractions() {
+        return attractions;
+    }
+
+    public Stall getStalls() {
+        return stalls;
+    }
+
+
 }
