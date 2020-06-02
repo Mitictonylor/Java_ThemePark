23e2685e9ba978921050e5c750fee454119046b2
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-02 17:13:34 +0100

    Test is allowed to in 3 different scenarios

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor15;
+    Visitor visitor8;
+    Visitor visitor19;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor15 = new Visitor(15,1.50, 20.00);
+        visitor8 = new Visitor(8,1.20, 15.00);
+        visitor19 = new Visitor(19,2.00, 30.00);
     }
 
     @Test
@@ -26,5 +33,18 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void canUseThePlayground__BelowMaxAge() {
+        assertTrue(playground.isAllowedTo(visitor8));
+    }
+    @Test
+    public void canUseThePlayground__ExactMaxAge() {
+        assertTrue(playground.isAllowedTo(visitor15));
+    }
+    @Test
+    public void canUseThePlayground__OverMaxAge() {
+        assertFalse(playground.isAllowedTo(visitor19));
     }
 }
